8d8b20b145afe649c6a987cef7c539550fc51166
unknown <yuenwal@.za.db.com>
2014-07-24 18:23:43 +0200

    wip

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Sub Heading 1
 
 ### Sub Sub Heading 1
 
+Instruction Set:
+1) a tidy data set as described below (tidy_data_set.rds file)
+2) a link to a Github repository with your script for performing the analysis: 
+	https://github.com/nom-de-plume/CleaningData_A1
+3) a code book that describes the variables, the data, and any transformations or work 
+	that you performed to clean up the data called CodeBook.md.
+4)  You should also include a README.md in the repo with your scripts. This repo explains 
+	how all of the scripts work and how they are connected. 
---
+++ run_analysis.R
@@ -0,0 +1,26 @@
+# Exploratory Data Analysis - Course Project 1 - plot1
+#   This is my work for the first assignment (plot 1)
+
+#Should move loading data to another function/script as it is common to all plots
+
+#load necessary libraries
+library(sqldf)
+
+#static variables
+doLoadData <- FALSE #choose whether to load the data from the file (just shortcut if data is already loaded)
+
+urlFileName <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+zipFileName <- "./getdata_projectfiles_UCI HAR Dataset.zip"
+csvFileName <- "UCI HAR Dataset"
+
+#begDate <- '1/2/2007' ##dd/mm/yyyy
+#endDate <- '2/2/2007' 
+
+# download and/or extract file if it does not exist
+if (!file.exists(zipFileName)) {
+	download.file(urlFileName, zipFileName, 'curl')
+}
+
+if (!file.exists(csvFileName)) {
+	unzip(zipFileName);
+}
