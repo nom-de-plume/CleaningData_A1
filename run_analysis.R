af0f847ad02e686d40c91684db2b45306fa783b6
Unknown <nom-de-plume@gmail.com>
2014-07-25 12:36:13 +0200

    wip

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,18 @@
 # Cleaning Data - Course Project 1 - plot1
-#   This is my work for the first assignment (plot 1)
-
-#Should move loading data to another function/script as it is common to all plots
+#   This is my work for the first assignment 
 
 #load necessary libraries
 #library(sqldf)
 
-#static variables
+## 01 - static variables
 doLoadData <- FALSE #choose whether to load the data from the file (just shortcut if data is already loaded)
 
 urlFileName <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
 zipFileName <- "./getdata_projectfiles_UCI HAR Dataset.zip"
 directoryName <- "./UCI HAR Dataset"
+
+
+dataHeadingsFileName <- paste(directoryName, "/features.txt",sep = "")
 
 testSubjectsFileName <- paste(directoryName, "/test/subject_test.txt",sep = "")
 testTypesFileName <- paste(directoryName, "/test/y_test.txt",sep = "")
@@ -22,15 +23,12 @@ trainTypesFileName <- paste(directoryName, "/train/y_train.txt",sep = "")
 trainDataSetFileName <- paste(directoryName, "/train/X_train.txt",sep = "")
 
 numColumnsDataSet <- 561
-numRowsDataSet <- 10 #For testing - should default to -1 for entire dataset
+numRowsDataSet <- -1 #For testing - should default to -1 for entire dataset
 
 columnHeaderTestType <- "TestType"
 columnHeaderSubject <- "Subject"
 
-#begDate <- '1/2/2007' ##dd/mm/yyyy
-#endDate <- '2/2/2007' 
-
-# download and/or extract file if it does not exist
+## 02 - download and/or extract file if it does not exist
 if (!file.exists(zipFileName)) {
 	download.file(urlFileName, zipFileName, 'curl')
 }
@@ -39,7 +37,12 @@ if (!file.exists(directoryName)) {
 	unzip(zipFileName);
 }
 
-# next load the individual datasets, merge, etc
+## 03 - next load the individual datasets, merge, etc
+
+dataHeadingsDataFrame <- read.table(dataHeadingsFileName)
+dataHeadingsVector <- dataHeadingsDataFrame[,2]
+
+## 03.01 - test dataset
 testSubjDataFrame <- read.table(testSubjectsFileName,
 	col.names = columnHeaderSubject,
 	nrows = numRowsDataSet)
@@ -51,10 +54,15 @@ testTypeDataFrame <- read.table(testTypesFileName,
 testDataFrame <- read.table(
 	file = testDataSetFileName,
 	colClasses = rep("numeric",numColumnsDataSet),
+	col.names = dataHeadingsVector,
 	nrows = numRowsDataSet)
 	
-testCompleteDataFrame <- cbind(testSubjDataFrame, testTypeDataFrame, testDataFrame)	
-  
+# only take the mean and std	
+testSubsetDataFrame <- testDataFrame[,grepl("mean\\(\\)|std\\(\\)", dataHeadingsVector)]
+	
+testCompleteDataFrame <- cbind(testSubjDataFrame, testTypeDataFrame, testSubsetDataFrame)	
+
+## 03.02 - train dataset
 trainSubjDataFrame <- read.table(trainSubjectsFileName,
 	col.names = columnHeaderSubject,
 	nrows = numRowsDataSet)
@@ -66,6 +74,15 @@ trainTypeDataFrame <- read.table(trainTypesFileName,
 trainDataFrame <- read.table(
 	file = trainDataSetFileName,
 	colClasses = rep("numeric",numColumnsDataSet),
+	col.names = dataHeadingsVector,	
 	nrows = numRowsDataSet)  
 	
-trainCompleteDataFrame <- cbind(trainSubjDataFrame, trainTypeDataFrame, trainDataFrame)		
+# only take the mean and std		
+trainSubsetDataFrame <- trainDataFrame[,grepl("mean\\(\\)|std\\(\\)", dataHeadingsVector)]
+	
+trainCompleteDataFrame <- cbind(trainSubjDataFrame, trainTypeDataFrame, trainSubsetDataFrame)		
+
+## 03.03 - merge both datasets
+allDataFrame <- rbind(testCompleteDataFrame, trainCompleteDataFrame)
+
+## 04 - create the average subset
