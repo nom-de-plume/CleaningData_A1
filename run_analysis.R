e0085ee7b2984e250c8605b03c88a873622a48e0
Unknown <nom-de-plume@gmail.com>
2014-07-25 10:58:29 +0200

    wip

--- run_analysis.R
+++ run_analysis.R
@@ -14,13 +14,18 @@ zipFileName <- "./getdata_projectfiles_UCI HAR Dataset.zip"
 directoryName <- "./UCI HAR Dataset"
 
 testSubjectsFileName <- paste(directoryName, "/test/subject_test.txt",sep = "")
+testTypesFileName <- paste(directoryName, "/test/y_test.txt",sep = "")
 testDataSetFileName <- paste(directoryName, "/test/X_test.txt",sep = "")
 
 trainSubjectsFileName <- paste(directoryName, "/train/subject_train.txt",sep = "")
+trainTypesFileName <- paste(directoryName, "/train/y_train.txt",sep = "")
 trainDataSetFileName <- paste(directoryName, "/train/X_train.txt",sep = "")
 
 numColumnsDataSet <- 561
 numRowsDataSet <- 10 #For testing - should default to -1 for entire dataset
+
+columnHeaderTestType <- "TestType"
+columnHeaderSubject <- "Subject"
 
 #begDate <- '1/2/2007' ##dd/mm/yyyy
 #endDate <- '2/2/2007' 
@@ -36,6 +41,11 @@ if (!file.exists(directoryName)) {
 
 # next load the individual datasets, merge, etc
 testSubjDataFrame <- read.table(testSubjectsFileName,
+	col.names = columnHeaderSubject,
+	nrows = numRowsDataSet)
+
+testTypeDataFrame <- read.table(testTypesFileName,
+	col.names = columnHeaderTestType,
 	nrows = numRowsDataSet)
 
 testDataFrame <- read.table(
@@ -43,9 +53,14 @@ testDataFrame <- read.table(
 	colClasses = rep("numeric",numColumnsDataSet),
 	nrows = numRowsDataSet)
 	
-testCompleteDataFrame <- cbind(testSubjDataFrame, testDataFrame)	
+testCompleteDataFrame <- cbind(testSubjDataFrame, testTypeDataFrame, testDataFrame)	
   
 trainSubjDataFrame <- read.table(trainSubjectsFileName,
+	col.names = columnHeaderSubject,
+	nrows = numRowsDataSet)
+  
+trainTypeDataFrame <- read.table(trainTypesFileName,
+	col.names = columnHeaderTestType,
 	nrows = numRowsDataSet)
   
 trainDataFrame <- read.table(
@@ -53,4 +68,4 @@ trainDataFrame <- read.table(
 	colClasses = rep("numeric",numColumnsDataSet),
 	nrows = numRowsDataSet)  
 	
-trainCompleteDataFrame <- cbind(trainSubjDataFrame, trainDataFrame)		
+trainCompleteDataFrame <- cbind(trainSubjDataFrame, trainTypeDataFrame, trainDataFrame)		
